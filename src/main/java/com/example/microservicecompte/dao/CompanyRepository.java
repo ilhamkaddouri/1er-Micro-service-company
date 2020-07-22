e69c3db05ae153eff2a87c006cddbb31555f5292
ilhamkaddouri <49375736+ilhamkaddouri@users.noreply.github.com>
2020-07-22 23:08:48 +0100

    Update CompanyRepository.java

--- src/main/java/com/example/microservicecompte/dao/CompanyRepository.java
+++ src/main/java/com/example/microservicecompte/dao/CompanyRepository.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.example.microservicecompte.entities.Company;
+//using the annotation @RepositoryRestResource to get ride of recreating my own crud rest function
+// the JpaRepositry is the class on behalf of making the communication with the BD 
 @RepositoryRestResource
 public interface CompanyRepository extends JpaRepository<Company,Long> {
 
